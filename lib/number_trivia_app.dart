57ef4f44bb3d6e545d347a01039adff46de041ff
radyhaggag <radyhaggag948@gmail.com>
2023-06-14 00:12:44 +0300

    clean main file

---
+++ lib/number_trivia_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NumberTriviaApp extends StatelessWidget {
+  const NumberTriviaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Number trivia',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:number_trivia_clean_architecture_tdd/main.dart';
+import 'package:number_trivia_clean_architecture_tdd/number_trivia_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NumberTriviaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
